55d2ef0757262f2f5017251124f0775690b4ffbb
Trouble-Andrew <satyr663@mail.ru>
2019-10-14 20:53:00 +0300

    Сверстал блоки приемущества и о компании.

--- gulpfile.js
+++ gulpfile.js
@@ -41,6 +41,7 @@ gulp.task("server", function () {
   gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
   gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
   gulp.watch("source/*.html", gulp.series("html", "refresh"));
+  // gulp.watch("source/js/**", gulp.series("js", "refresh"));
 });
 
 gulp.task("refresh", function (done) {
@@ -79,6 +80,15 @@ gulp.task("html", function () {
       include()
     ]))
     .pipe(gulp.dest("build"));
+});
+
+gulp.task("js", function () {
+  return gulp.src([
+    "source/js/**"
+    ], {
+      base: "source"
+    })
+  .pipe(gulp.dest("build"));
 });
 
 gulp.task("copy", function () {
--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,30 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+// var pageHeader = document.querySelector('.page-header');
+// var headerToggle = document.querySelector('.page-header__toggle');
+//
+// pageHeader.classList.remove('page-header--nojs');
+//
+// headerToggle.addEventListener('click', function () {
+//   if (pageHeader.classList.contains('page-header--closed')) {
+//     pageHeader.classList.remove('page-header--closed');
+//     pageHeader.classList.add('page-header--opened');
+//   } else {
+//     pageHeader.classList.add('page-header--closed');
+//     pageHeader.classList.remove('page-header--opened');
+//   }
+// });
 
-pageHeader.classList.remove('page-header--nojs');
+var screen = document.body.clientWidth;
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-});
+var companyText = document.querySelectorAll('.company__descr');
+var text = companyText[companyText.length - 1].innerHTML;
+console.log(text);
+console.log(text.split(' ').splice(0, 23).join(' ') + '...');
+
+
+if (screen < 1024) {
+  var companyText = document.querySelectorAll('.company__descr');
+  var text = companyText[companyText.length - 1].innerHTML;
+  companyText[companyText.length - 1].innerHTML = text.split(' ').splice(0, 23).join(' ') + '..';
+  // console.log(text.split(' ').splice(0, 23).join(' ') + '...');
+}

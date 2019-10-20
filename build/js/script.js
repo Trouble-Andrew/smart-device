8dee8a786c8618795c44ca025586f95926e180bc
Trouble-Andrew <satyr663@mail.ru>
2019-10-20 18:16:11 +0300

    Исправление скрипта маски.

--- build/js/script.js
+++ build/js/script.js
@@ -171,22 +171,22 @@ increaseSectionWidth();
 var phoneInput = document.querySelector('#phone');
 var modalphoneInput = document.querySelector('#modal-phone');
 
-var getPhoneMask = function () {
+var getPhoneMask = function (event) {
   if (!(event.key === 'ArrowLeft' || event.key === 'ArrowRight' || event.key === 'Backspace' || event.key === 'Tab')) {
     event.preventDefault();
   }
   var mask = '+7 (111) 111-11-11';
 
   if (/[0-9\+\ \-\(\)]/.test(event.key)) {
-    var currentString = this.value;
+    var currentString = event.target.value;
     var currentLength = currentString.length;
     if (/[0-9]/.test(event.key)) {
       if (mask[currentLength] === '1') {
-        this.value = currentString + event.key;
+        event.target.value = currentString + event.key;
       } else {
         for (var i = currentLength; i < mask.length; i++) {
           if (mask[i] === '1') {
-            this.value = currentString + event.key;
+            event.target.value = currentString + event.key;
             break;
           }
           currentString += mask[i];
